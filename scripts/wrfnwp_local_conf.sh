2070f7889e6900cfdec2196d1804dfc1e2425400
Michal Belda <micval@centrum.cz>
2017-01-16 10:32:25 +0100

    global settings of time variables

--- scripts/wrfnwp_local_conf.sh
+++ scripts/wrfnwp_local_conf.sh
@@ -10,4 +10,19 @@ export GLOBAL_MODEL=GFS
 if check_true_value $SIMULATION_OFFLINE; then
     print_log_message "Setting up offline simulation parameters..."
     # fill in required parameters settings
+else
+    y=$(date '+%Y')
+    m=$(date '+%m')
+    d=$(date '+%d')
+    h=00
+
+    ym=$(date --date=1day '+%Y')
+    mm=$(date --date=1day '+%m')
+    dm=$(date --date=1day '+%d')
+    hm=00
+
+    ye=$(date --date=2day '+%Y')
+    me=$(date --date=2day '+%m')
+    de=$(date --date=2day '+%d')
+    he=00
 fi
--- scripts/wrfnwp_run_cecilia_domain_10km.sh
+++ scripts/wrfnwp_run_cecilia_domain_10km.sh
@@ -15,21 +15,6 @@ export MY_DATA_DIR
 
 print_log_message "Starting job $0: $PROJECT $EXP run"
 
-y=$(date '+%Y')
-m=$(date '+%m')
-d=$(date '+%d')
-h=00
-
-ym=$(date --date=1day '+%Y')
-mm=$(date --date=1day '+%m')
-dm=$(date --date=1day '+%d')
-hm=00
-
-ye=$(date --date=2day '+%Y')
-me=$(date --date=2day '+%m')
-de=$(date --date=2day '+%d')
-he=00
-
 . ./wrfnwp_wps.sh
 
 . ./wrfnwp_model.sh
